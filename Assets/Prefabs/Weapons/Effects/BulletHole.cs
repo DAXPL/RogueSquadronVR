3881ec0761ff057c44be71b5d58c77faa773d853
Miłosz Klim <klim.milosz@gmail.com>
2024-09-18 11:30:45 +0200

    Material effects

--- Assets/Prefabs/Weapons/Effects/BulletHole.cs
+++ Assets/Prefabs/Weapons/Effects/BulletHole.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class BulletHole : MonoBehaviour
 {
+    [SerializeField] private SurfaceData defaultSurfaceData;
+    private SurfaceData surfaceData;
     private void Start()
     {
-        Destroy(this,30);
+        RaycastHit hit;
+        GameObject g;
+        if (Physics.Raycast(transform.position,transform.forward,out hit) == false) return;
+        if(hit.collider.TryGetComponent(out SurfaceInfo surface))
+            g =Instantiate(surface.GetSurfaceData().GetHitEffects(), transform.position, transform.rotation, hit.collider.transform);
+        else
+            g = Instantiate(defaultSurfaceData.GetHitEffects(), transform.position, transform.rotation, hit.collider.transform);
+        Destroy(g,30);
+        Destroy(this);
+
     }
 }
