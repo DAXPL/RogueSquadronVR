80dd280df2a882d3637a814ce60826c3910f6f2f
Miłosz Klim <klim.milosz@gmail.com>
2024-09-28 14:47:11 +0200

    Lirwen rain effect

--- Assets/Scripts/Planetmanager.cs
+++ Assets/Scripts/Planetmanager.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Planetmanager : MonoBehaviour
+public class Planetmanager : NetworkBehaviour
 {
     [SerializeField] private string planetName;
+    [SerializeField] private GameObject rainEffect;
+    private NetworkVariable<bool> isRaining = new NetworkVariable<bool>();
     void Start()
     {
-        //SceneManager.SetActiveScene(SceneManager.GetSceneByName(planetName));
         Debug.Log($"Welcome to {planetName}");
+        isRaining.OnValueChanged += OnRainStateChanged;
+        if (IsOwner || IsServer) OnPlayersArriveServerRpc();
     }
+
+    private void OnRainStateChanged(bool previousValue, bool newValue)
+    {
+        if(rainEffect)rainEffect.SetActive(newValue);
+    }
+
+    [ServerRpc]
+    private void OnPlayersArriveServerRpc()
+    {
+        isRaining.Value = Random.Range(0.0f, 1.0f) > 0.5f ? true:false ;
+    }
+
 }
