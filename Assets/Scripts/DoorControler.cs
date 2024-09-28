165bd9e1d5d60380a09c3f88608bb5a578009861
Miłosz Klim <klim.milosz@gmail.com>
2024-09-28 21:31:44 +0200

    Sounds

--- Assets/Scripts/DoorControler.cs
+++ Assets/Scripts/DoorControler.cs
@@ -8,23 +8,27 @@ public class DoorControler : NetworkBehaviour
 {
     private NetworkVariable<bool> doorState = new NetworkVariable<bool>(false,NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Server);
     private Animator animator;
-
+    private AudioSource audioSource;
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
         doorState.OnValueChanged += onDoorstateChanged;
     }
 
     private void onDoorstateChanged(bool previousValue, bool newValue)
     {
-        animator.SetBool("DoorState",newValue);
+        if(previousValue == newValue) return;
+        if(animator)animator.SetBool("DoorState",newValue);
+        if(audioSource) audioSource.Play();
     }
 
     public void SetDoorState(bool newState)
     {
         if(IsServer) doorState.Value = newState;
     }
+    
     public void ToggleDoorState()
     {
         if (IsServer) doorState.Value = !doorState.Value;
