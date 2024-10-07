74594d9898a6980e80d2a1c26ad3a5deded94111
Miłosz Klim <klim.milosz@gmail.com>
2024-10-07 07:13:35 +0200

    Base water os

---
+++ Assets/Prefabs/Aridis/WaterPumpMission/WaterFarmController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFarmController : Serviceable
+{
+    [SerializeField] private WaterPumpController[] waterPumps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
