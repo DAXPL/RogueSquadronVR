4a6d2b7edfda9146caa5909de4a28016f46c3b4d
Miłosz Klim <klim.milosz@gmail.com>
2024-11-06 00:01:32 +0100

    Base mortar

---
+++ Assets/Prefabs/Weapons/ShipMortar/MortarSystem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortarSystem : MonoBehaviour
+{
+    [SerializeField] private float mortarDelay = 5;
+    [SerializeField] private Transform croshair;
+    private Vector3 moveVector = new Vector3(0, 0, 0);
+    private float sensitivity = 1;
+
+    private float mortarShooTimestamp = 0;
+    public void MoveX(float delta)
+    {
+        moveVector.x = delta;
+    }
+    public void MoveZ(float delta)
+    {
+        moveVector.z = delta;
+    }
+
+    public void Fire()
+    {
+        if(Time.time <= mortarShooTimestamp + mortarDelay) return;
+        Debug.Log("Mortar fire");
+        mortarShooTimestamp = Time.time;
+    }
+
+    private void Update()
+    {
+        if(croshair == null) return;
+        croshair.transform.Translate(moveVector*Time.deltaTime);
+    }
+}
