7e11aa80300ca05efb87e728d7c85416c7f6be02
Miłosz Klim <klim.milosz@gmail.com>
2024-11-15 11:35:03 +0100

    Many small fixes

--- Assets/Prefabs/Weapons/Projectile/Projectile.cs
+++ Assets/Prefabs/Weapons/Projectile/Projectile.cs
@@ -10,11 +10,9 @@ public class Projectile : NetworkBehaviour
 {
     [SerializeField] private LayerMask CollisionMask;
     [SerializeField] private SurfaceData defaultSurfaceData;
-    [SerializeField] private int maxReflections = 1;
 
     private int damage = 10;
     private int force = 10;
-    private int reflections = 0;
     private Rigidbody rb;
 
     private NetworkObject no;
@@ -46,7 +44,6 @@ public class Projectile : NetworkBehaviour
 
         if (collision.transform.CompareTag("Reflection"))
         {
-            reflections++;
             rb.velocity = Vector3.Reflect(rb.velocity, collision.contacts[0].normal).normalized * force;
         }
         else
